57166fbcbd04c641546664977df0af1d1bac4a52
Kyle Chriscoe <kyle717@gmail.com>
2021-05-04 17:41:24 -0400

    Add function to assemble full elven name

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const parentNames = ["jon", "alex"];
+const parentNames = ["Aegn", "Amar", "Cele", "Daer", "Ecthel", "Eol", "Gil"];
 const firstNameEndings = ["ien", "iel", "ion"];
-const secondNames = ["middle"];
-const thirdNames = ["third"];
+const secondNames = ["Ambarussa", "Arakáno", "Ingoldo", "Turcáno"];
+const thirdNames = ["Maedhros", "Gil-galad", "Cirdan", "Felagund"];
 
 function randomComponent(componentList) {
     // Function that returns a random name component from a given array
     // Input: one arg; the array to select a component from
     // Output: String
+
     const index = Math.floor(Math.random() * componentList.length);
     return componentList[index];
 }
@@ -15,10 +16,23 @@ function createFirstName() {
     // Function that returns a randomly assembled First name from parent name array and first name endings
     // Input: n/a
     // Output: String
+
     const pName = randomComponent(parentNames);
     const ending = randomComponent(firstNameEndings);
 
     return pName + ending;
 }
 
-console.log(createFirstName());
+function createFullName() {
+    // Function that returns a randomly generated name with first, second and third names
+    // Input: n/a
+    // Output: String
+
+    const fName = createFirstName();
+    const mName = randomComponent(secondNames);
+    const lName = Math.floor(Math.random() * 2) === 1 ? randomComponent(thirdNames) : "";
+
+    return `${fName} ${mName} ${lName}`;
+}
+
+console.log(createFullName());
